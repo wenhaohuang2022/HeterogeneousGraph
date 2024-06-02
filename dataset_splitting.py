ac2abeb85e5faf279fa402420656c3068f6f3fc5
Wenhao Huang <2068219707@qq.com>
2024-06-02 09:00:07 +0800

    update dataset_splitting.py and load_file.py

--- dataset_splitting.py
+++ dataset_splitting.py
@@ -1,25 +1,26 @@
 import torch
 from torch.utils.data import DataLoader, random_split
 import dgl
-from main import dataset
 
-# 定义划分的比例
-train_ratio = 0.7
-validate_ratio = 0.15
-test_ratio = 0.15
-# 计算划分的样本数量
-num_data = len(dataset)
-num_train = int(train_ratio * num_data)
-num_validate = int(validate_ratio * num_data)
-num_test = num_data - num_train - num_validate
 
-# 使用 random_split 函数划分数据集
-train_dataset, val_dataset, test_dataset = random_split(dataset, [num_train, num_validate, num_test])
+def collate_fn(batch):
+    graphs, labels = zip(*batch)
+    batched_graph = dgl.batch(graphs)
+    batched_labels = torch.stack(labels)
+    return batched_graph, batched_labels
 
-# 创建数据加载器
-batch_size = 32
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-# 划分完成
+def dataset_split(dataset, train_ratio=0.7, validate_ratio=0.15, batch_size=32, collate_fn=collate_fn):
+    num_data = len(dataset)
+    num_train = int(train_ratio * num_data)
+    num_validate = int(validate_ratio * num_data)
+    num_test = num_data - num_train - num_validate
+
+    # 使用 random_split 函数划分数据集
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [num_train, num_validate, num_test])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+    # 划分完成
+    return train_loader, val_loader, test_loader
--- load_file.py
+++ load_file.py
@@ -4,23 +4,30 @@ import os
 from torch.utils.data import DataLoader
 from main import load_dataset, save_path
 from visualization import plot_heterograph
-def collate_fn(batch):
-    graphs, labels = zip(*batch)
-    batched_graph = dgl.batch(graphs)
-    batched_labels = torch.stack(labels)
-    return batched_graph, batched_labels
+from dataset_splitting import dataset_split, collate_fn
 
 loaded_dataset = load_dataset(save_path)
-dataloader = DataLoader(loaded_dataset, batch_size=8, shuffle=True, collate_fn=collate_fn)
-for batched_g, batched_l in dataloader:
+train_loader, validate_loader, test_loader = dataset_split(loaded_dataset, collate_fn=collate_fn)
+
+for batched_g, batched_l in train_loader:
+    print('train dataset:')
+    print(batched_g)
+    print(batched_l)
+    break
+for batched_g, batched_l in validate_loader:
+    print('validate dataset:')
+    print(batched_g)
+    print(batched_l)
+    break
+for batched_g, batched_l in test_loader:
+    print('test dataset:')
     print(batched_g)
     print(batched_l)
     break
 
-output_dir = 'graph_images'
-for i in range(0,307):
-    graph_example,label_example = dataloader.dataset.__getitem__(i)
-    plot_heterograph(graph_example,num=i,plot=False,save=True)
-
-
-
+'''visualizing the graphs'''
+# output_dir = 'graph_images'
+dataloader = DataLoader(loaded_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
+for i in range(0, 2):
+    graph_example, label_example = dataloader.dataset.__getitem__(i)
+    plot_heterograph(graph_example, num=i, plot=True, save=False)
